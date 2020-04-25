12ae326f75fd1c4f4435ddc300e954b3254fbd0c
codenuk <supagorn00001@gmail.com>
2020-04-25 13:00:16 +0000

    test

---
+++ src/js/components/work-order/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import NavTopbar from '../nav/nav-top.js';
+import TopContent from './top-content';
+import BottomContent from './bottom-content';
+import NavBottom from '../nav/nav-bottom.js';
+
+class WO extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavTopbar />
+                <TopContent />
+                <BottomContent />
+                <NavBottom />
+            </div>
+        )
+    };
+}
+
+export default WO;
