4bc55d9107fad504411c113ea45321df344d3abf
codenuk <supagorn00001@gmail.com>
2020-04-27 12:42:56 +0000

    update

---
+++ src/js/components/nav/nav-toolbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import AddDocument from '../../../images/toolbar/add-document.svg'
+import CopyDocument from '../../../images/toolbar/copy-document.svg'
+import SaveDocument from '../../../images/toolbar/save.svg'
+import RetryDocument from '../../../images/toolbar/retry.svg'
+import BackDocument from '../../../images/toolbar/back.svg'
+import ForwardDocument from '../../../images/toolbar/forward.svg'
+import PdfDocument from '../../../images/toolbar/pdf.svg'
+
+import '../../../vender/fontawesome-free/css/all.css';
+import '../../../css/style-nav.css'
+
+class NavTop extends React.Component {
+    render() {
+        return (
+            <div>
+                <div id="toolbar">
+                    <div className="container_12 clearfix">
+                        <ul className="grid_12 nav-ul">
+                            <li className="nav-li" ><Link to="/main"><i className="fas fa-home icon-toolbar-first" ></i></Link></li>
+                            <li className="nav-li" ><Link to="/main"><i className="fas fa-search icon-toolbar" ></i></Link></li>
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={AddDocument} className="img-toolbar" /></Link></li>
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={CopyDocument} className="img-toolbar" /></Link></li>
+
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={SaveDocument} className="img-toolbar" /></Link></li>
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={RetryDocument} className="img-toolbar" /></Link></li>
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={BackDocument} className="img-toolbar" /></Link></li>
+
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={ForwardDocument} className="img-toolbar" /></Link></li>
+                            <li className="nav-li" ><Link to="/main" className="toolbar"><img alt='AddDocument' src={PdfDocument} className="img-toolbar" /></Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+}
+
+
+export default NavTop;
--- src/js/components/work-request/index.js
+++ src/js/components/work-request/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import NavTopbar from '../nav/nav-top.js';
+import ToolBar from '../nav/nav-toolbar.js';
 import TopContent from './top-content';
 import BottomContent from './bottom-content';
 import NavBottom from '../nav/nav-bottom.js';
@@ -11,6 +12,7 @@ class WO extends React.Component {
         return (
             <div>
                 <NavTopbar />
+                <ToolBar />
                 <TopContent />
                 <BottomContent />
                 <NavBottom type={type} />
