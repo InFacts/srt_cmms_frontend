d08b35212240aed6589112a842e05be8421cd302
codenuk <supagorn00001@gmail.com>
2020-05-19 14:58:06 +0000

    .

--- src/js/components/spare-good-receipt/wrap-form.js
+++ src/js/components/spare-good-receipt/wrap-form.js
@@ -7,9 +7,28 @@ import Footer from '../common/footer.js';
 
 class WrapForm extends React.Component {
 
+    handleSubmit = event => {
+        event.preventDefault();
+
+        // const editCategory = {
+        //     "name": this.state.name,
+        //     "description": this.state.description
+        // };
+        // console.log("editCategory", editCategory)
+
+        // axios.put(`http://vanilla-erp.com:${API_URL_DATABASE}/api/v1/product_categories/${this.props.match.params.product_category_id}`, editCategory, { headers: { "x-access-token": localStorage.getItem('token_auth') } })
+        //     .then(res => {
+        //         // console.log(res);
+        //         this.setState({ isFinish: true });
+        //     }).catch(function (err) {
+        //         console.log(err);
+        //     })
+
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <TopContent />
                 <BottomContent />
                 <Footer />
