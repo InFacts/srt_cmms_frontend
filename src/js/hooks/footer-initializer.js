3c7d920b8c714aa4eeabe62d2438054f3fe1bfa6
sorawit <sorawit@server>
2020-06-04 03:27:15 +0000

    TODO: Approval & Track Document

--- src/js/hooks/footer-initializer.js
+++ src/js/hooks/footer-initializer.js
@@ -29,15 +29,63 @@ const useFactInitializer = () => {
             dispatch(footerToModeAddDraft());
         }
         else if (internal_document_id !== "") {
+            // action_approval
+            // GET APPROVAL STEP http://43.229.79.36:60013/approval/{document_id}/all
+            // For Loop find "approval_step.approval_by" & "position_id" & "user_id" & Check "isCancel" & "approval_status_id"
+            let userID = 0;
+            let approvalStatusID = 0;
+            let userHasPosition = [];
             // GET Infomation of user
+            // TODO if Fact. it has user_id, position_id, warehouse_id, position_group, level_id, has_positions. You can remove API /user/profile
             axios.get(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/user/profile`, { headers: { "x-access-token": localStorage.getItem('token_auth') } }).then((resUser) => {
+                userID = resUser.data.user_id;
                 const userLevelID = resUser.data.level_id;
-                const userHasPosition = resUser.data.has_positions;// abbreviation: "สสญ.นว.", name: "สารวัตรงานบำรุงรักษาอาณัติสัญญาณแขวงนครสวรรค์", position_group_id: 3, position_id: 33, warehouse_id: null
+                userHasPosition = resUser.data.has_positions;// abbreviation: "สสญ.นว.", name: "สารวัตรงานบำรุงรักษาอาณัติสัญญาณแขวงนครสวรรค์", position_group_id: 3, position_id: 33, warehouse_id: null
                 axios.get(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/approval/${document_id}/all`, { headers: { "x-access-token": localStorage.getItem('token_auth') } }).then((res) => {
-                    console.log("DONUT TEST", res.data.results)
-                    // action_approval
-                    // GET APPROVAL STEP http://43.229.79.36:60013/approval/{document_id}/all
-                    // For Loop find "approval_step.approval_by" & "position_id" & "user_id" & Check "isCancel" & "approval_status_id"
+                    // console.log("DONUT TEST", res.data.results, userHasPosition)
+                    // Check Previous Approver 
+                    let approvalFlowBy = res.data.results;
+                    approvalFlowBy[0].approval_step.map(approvalStep => {
+                        console.log("approvalStep", approvalStep)
+                        if (approvalStep.is_cancel === 0 || approvalStep.is_cancel === undefined) {
+                            if (approvalStep.approval_by.user_id === userID) {
+                                approvalStatusID = approvalStep.approval_status_id;
+                                console.log("approvalStatusID", approvalStatusID)
+                            }
+                            else {
+                                console.log("General User for view")
+                            }
+                        }
+                    });
+                    
+                    // Check Next Approver
+                    axios.get(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/approval/${document_id}/latest/step`, { headers: { "x-access-token": localStorage.getItem('token_auth') } }).then((res) => {
+                    // console.log("DONUT TEST", res.data.results, userHasPosition)
+                    // Check Previous Approver 
+                    let approvalFlowBy = res.data.results;
+                    approvalFlowBy[0].approval_step.map(approvalStep => {
+                        console.log("approvalStep", approvalStep)
+                        if (approvalStep.is_cancel === 0 || approvalStep.is_cancel === undefined) {
+                            if (approvalStep.approval_by.user_id === userID) {
+                                approvalStatusID = approvalStep.approval_status_id;
+                                console.log("approvalStatusID", approvalStatusID)
+                            }
+                            else {
+                                console.log("General User for view")
+                            }
+                        }
+                    });
+                    
+                    // Check Next Approver
+
+                    
+
+                }).catch(function (err) {
+                    console.log(err)
+                })
+
+                    
+
                 }).catch(function (err) {
                     console.log(err)
                 })
