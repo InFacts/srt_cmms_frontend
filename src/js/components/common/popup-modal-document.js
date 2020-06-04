2e1389632e3d6d50e937b27827bbce090a6f49db
codenuk <supagorn00001@gmail.com>
2020-06-04 20:00:21 +0000

    finish page goods receipt, good receipt no po and  goods return

--- src/js/components/common/popup-modal-document.js
+++ src/js/components/common/popup-modal-document.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 import axios from "axios";
 import { API_PORT_DATABASE } from '../../config_port.js';
@@ -7,11 +7,11 @@ import { API_URL_DATABASE } from '../../config_url.js';
 // import {handleChange} from '../../redux/modules/form_data.js';
 import { useFormikContext } from 'formik';
 
-const PopupModalDocument = () => {
+const PopupModalDocument = (props) => {
     const [data, setData] = useState([]);
     const [documentID, setDocumentID] = useState("");
-    const [url, setUrl] = useState(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/document/search?document_type_group_id=101&internal_document_id=${documentID}`)
-    const {setFieldValue} = useFormikContext();
+    const [url, setUrl] = useState(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/document/search?document_type_group_id=${props.documentTypeGroupID}&internal_document_id=${documentID}`)
+    const { setFieldValue } = useFormikContext();
 
     useEffect(() => {
         const fetchData = () => {
@@ -19,67 +19,55 @@ const PopupModalDocument = () => {
                 .then((res) => {
                     setData(res.data.results);
                 })
-        }; 
+        };
         fetchData();
     }, [url]);
 
-    function onClickSelect(e) {
-        e.preventDefault()
-        setFieldValue('internal_document_id', document.internal_document_id, true); // Force Trigger Validation onClick
-    }
-
     return (
-    <div className="modal" id="modalDocument" style={{ display: "none" }}>
-        <div className="gray-board">
-        <p className="head-title-modal edit">ค้นหาเลขที่เอกสาร</p>
-        <div className="container_12 edit-padding">
+        <div className="modal" id={props.id} style={{ display: "none" }}>
+            <div className="gray-board">
+                <p className="head-title-modal edit">ค้นหาเลขที่เอกสาร</p>
+                <div className="container_12 edit-padding">
 
-            <div className="container_12">
-            <div className="grid_2"><p className="cancel-default">เลขที่เอกสาร</p></div>
-            <div className="grid_8 pull_0">
-                <input type="text" className="cancel-default grid_3" value={documentID} onChange={e => setDocumentID(e.target.value)} />
-                <button className="button-blue edit grid_1 mr-5" type="button" onClick={() => setUrl(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/document/search?document_type_group_id=101&internal_document_id=${documentID}`)}>ค้นหา</button>
+                    <div className="container_12">
+                        <div className="grid_2"><p className="cancel-default">เลขที่เอกสาร</p></div>
+                        <div className="grid_8 pull_0">
+                            <input type="text" className="cancel-default grid_3" value={documentID} onChange={e => setDocumentID(e.target.value)} />
+                            <button className="button-blue edit grid_1 mr-5" type="button" onClick={() => setUrl(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/document/search?document_type_group_id=${props.documentTypeGroupID}&internal_document_id=${documentID}`)}>ค้นหา</button>
+                        </div>
+                    </div>
+
+                    <div className="container_12">
+                        <table className="table-many-column mt-3">
+                            <thead>
+                                <tr>
+                                    <th className="font" style={{ minWidth: "300px" }}>เลขที่เอกสาร</th>
+                                    <th className="font" style={{ minWidth: "450px" }}>สร้างวันที่</th>
+                                    <th className="font" style={{ minWidth: "150px" }}>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map(function (document, index) {
+                                    return (
+                                        <tr key={index} id={index}>
+                                            <td className="edit-padding" style={{ minWidth: "150px" }}> {document.internal_document_id} </td>
+                                            <td className="edit-padding" style={{ minWidth: "300px" }}> {document.created_on.replace("T", " เวลา ").slice(0, 21) + " น."} </td>
+                                            <td className="edit-padding text-center" style={{ minWidth: "150px" }}>
+                                                <button type="button" className="button-blue" onClick={() => setFieldValue(`${props.name}`, document.internal_document_id, true)} aria-label="Close active modal" aria-controls={props.id} >เลือก</button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <button className="button-blue float-right grid_1 mr-5 mt-3" type="button" aria-label="Close active modal" aria-controls={props.id}>กลับ</button>
+
+                </div>
             </div>
-            </div>
-
-            <div className="container_12">
-            <table className="table-many-column mt-3">
-                <thead>
-                <tr>
-                    <th className="font" style={{ minWidth: "300px" }}>เลขที่เอกสาร</th>
-                    <th className="font" style={{ minWidth: "450px" }}>สร้างวันที่</th>
-                    <th className="font" style={{ minWidth: "150px" }}>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map(function (document, index) {
-                    return (
-                    <tr key={index} id={index}>
-                        <td className="edit-padding" style={{ minWidth: "150px" }}> {document.internal_document_id} </td>
-                        <td className="edit-padding" style={{ minWidth: "300px" }}> {document.created_on.replace("T", " เวลา ").slice(0, 21) + " น."} </td>
-                        <td className="edit-padding text-center" style={{ minWidth: "150px" }}>
-                        {/* <button type="button" className="button-blue" onClick={() => props.handleChange("document_id", document.document_id)} aria-label="Close active modal" aria-controls="modalDocument" id="closeModalInventory" >เลือก</button> */}
-                        {/* <button type="button" className="button-blue" onClick={() => setFieldValue('internal_document_id', document.internal_document_id, true)} aria-label="Close active modal" aria-controls="modalDocument" >เลือก</button> */}
-                        <button type="button" className="button-blue" onClick={() => setFieldValue('internal_document_id', document.internal_document_id, true)} aria-label="Close active modal" aria-controls="modalDocument" >เลือก</button>
-                        </td>
-                    </tr>
-                    )
-                })}
-                </tbody>
-            </table>
-            </div>
-
-            <button className="button-blue float-right grid_1 mr-5 mt-3" type="button" aria-label="Close active modal" aria-controls="modalDocument">กลับ</button>
-
         </div>
-        </div>
-    </div>
-)
+    )
 }
 
-// const mapStateToProps = null; 
-
-// const mapDispatchToProps = {
-//     handleChange
-// }
 export default PopupModalDocument;
---
+++ src/js/components/spare-goods-receipt2/top-content.js
@@ -0,0 +1,286 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
+
+import axios from "axios";
+import { API_PORT_DATABASE } from '../../config_port.js';
+import { API_URL_DATABASE } from '../../config_url.js';
+import { v4 as uuidv4 } from 'uuid';
+
+import FormInput from '../common/form-input'
+import TextInput from '../common/formik-text-input'
+import DateTimeInput from '../common/formik-datetime-input'
+import DateInput from '../common/formik-date-input'
+
+import { useFormikContext, useField } from 'formik';
+
+import PopupModalDocument from '../common/popup-modal-document'
+import PopupModalInventory from '../common/popup-modal-inventory'
+import PopupModalUsername from '../common/popup-modal-username'
+import { TOOLBAR_MODE, TOOLBAR_ACTIONS, toModeAdd } from '../../redux/modules/toolbar.js';
+import { getEmployeeIDFromUserID, fetchStepApprovalDocumentData, DOCUMENT_TYPE_ID } from '../../helper';
+
+
+const responseToFormState = (userFact, data, step_approve, desrciption_files) => {
+  for (var i = data.line_items.length; i <= 9; i++) {
+    data.line_items.push(
+      {
+        item_id: "",
+        internal_item_id: "",
+        description: "",
+        quantity: "",
+        uom_group_id: "",
+        unit: "",
+        per_unit_price: "",
+        list_uoms: []
+      }
+    );
+  }
+  return {
+    internal_document_id: data.internal_document_id,
+    created_by_user_employee_id: getEmployeeIDFromUserID(userFact, data.created_by_user_id) || '',
+    created_by_admin_employee_id: getEmployeeIDFromUserID(userFact, data.created_by_admin_id) || '',
+    created_on: data.created_on.split(".")[0],
+    line_items: data.line_items,
+    dest_warehouse_id: data.dest_warehouse_id,
+    remark: data.remark,
+    status_name_th: data.status_name,
+    po_id: data.po_id,
+
+    // Setup value From Approve and Attachment
+    step_approve: step_approve.approval_step === undefined ? [] : step_approve.approval_step,
+    desrciption_files_length: desrciption_files.length,
+    desrciption_files: desrciption_files
+  }
+}
+
+
+
+
+const TopContent = (props) => {
+  const { values, errors, touched, setFieldValue, handleChange, handleBlur, getFieldProps, setValues, validateField, validateForm } = useFormikContext();
+
+  // Fill Default Forms
+  useEffect(() => {
+    if (props.toolbar.mode === TOOLBAR_MODE.ADD) {
+      if (!values.internal_document_id && touched.internal_document_id){
+        setFieldValue('internal_document_id', `draft-${uuidv4()}`)
+      }
+      setFieldValue("created_by_admin_employee_id", getEmployeeIDFromUserID(props.fact.users, props.decoded_token.id));
+      setFieldValue("status_name_th", "ยังไม่ได้รับการบันทึก");
+      setFieldValue("created_on", new Date().toISOString().slice(0, 16));
+      // validateField("created_by_admin_employee_id");
+    }
+  }, [props.decoded_token, props.fact.users, props.toolbar.mode, touched.internal_document_id, !values.internal_document_id])
+
+  const validateInternalDocumentIDField = internal_document_id => new Promise(resolve => {
+    // Internal Document ID
+    //  {DocumentTypeGroupAbbreviation}-{WH Abbreviation}-{Year}-{Auto Increment ID}
+    //  ie. GR-PYO-2563/0001
+    console.log("I am validating document id")
+    let internalDocumentIDRegex = /^(GP|GT|GR|GU|GI|IT|GX|GF|PC|IA|SR|SS)-[A-Z]{3}-\d{4}\/\d{4}$/g
+    let draftInternalDocumentIDRegex = /^draft-\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b$/g
+    // let draftInternalDocumentIDRegex = /^heh/g
+    if (!internal_document_id) {
+      return resolve('Required');
+    } else if (!internalDocumentIDRegex.test(internal_document_id) && !draftInternalDocumentIDRegex.test(internal_document_id)) { //
+      return resolve('Invalid Document ID Format\nBe sure to use the format ie. GR-PYO-2563/0001')
+    }
+    // if (!internal_document_id) {
+    //   return resolve(); // Resolve doesn't return
+    // }
+    let error;
+    const url = `http://${API_URL_DATABASE}:${API_PORT_DATABASE}/document/internal_document_id/${encodeURIComponent(internal_document_id)}`;
+    axios.get(url, { headers: { "x-access-token": localStorage.getItem('token_auth') } })
+      .then((res) => {
+        if (res.data.internal_document_id === internal_document_id) { // If input document ID exists
+          if (props.toolbar.mode === TOOLBAR_MODE.SEARCH && !props.toolbar.requiresHandleClick[TOOLBAR_ACTIONS.ADD]) { //If Mode Search, needs to set value 
+
+            // Start Axios Get step_approve and attachment By nuk
+            // axios.get(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/approval/${res.data.document_id}/latest/plus`, { headers: { "x-access-token": localStorage.getItem('token_auth') } })
+            //   .then((step_approve) => {
+              fetchStepApprovalDocumentData(res.data.document_id)
+              .then((result) => {
+                axios.get(`http://${API_URL_DATABASE}:${API_PORT_DATABASE}/attachment/${res.data.document_id}`, { headers: { "x-access-token": localStorage.getItem('token_auth') } })
+                  .then((desrciption_files) => {
+                    console.log(" I AM STILL IN MODE SEARCH AND SET VALUE")
+                    setValues({ ...values, ...responseToFormState(props.fact.users, res.data, result, desrciption_files.data.results) }, false); //Setvalues and don't validate
+                    validateField("dest_warehouse_id");
+                    validateField("created_by_user_employee_id");
+                    validateField("created_by_admin_employee_id");
+                    // validateField("internal_document_id");
+                    return resolve(null);
+                  });
+                
+              })
+              
+              // });
+            // End
+
+          } else { //If Mode add, need to error duplicate Document ID
+            console.log("I AM DUPLICATE")
+            error = 'Duplicate Document ID';
+          }
+        } else { // If input Document ID doesn't exists
+          if (props.toolbar.mode === TOOLBAR_MODE.SEARCH) { //If Mode Search, invalid Document ID
+            console.log("I KNOW IT'sINVALID")
+            error = 'Invalid Document ID';
+          } else {//If mode add, ok
+          }
+        }
+      })
+      .catch((err) => { // 404 NOT FOUND  If input Document ID doesn't exists
+        if (props.toolbar.mode === TOOLBAR_MODE.SEARCH) { //If Mode Search, invalid Document ID
+          error = 'Invalid Document ID';
+        }//If mode add, ok
+      })
+      .finally(() => {
+        return resolve(error)
+      });
+  });
+
+  const validateEmployeeIDField = (fieldName, employee_id) => {
+    console.log("I am validating employee id")
+    employee_id = employee_id.split('\\')[0]; // Escape Character USERNAME CANT HAVE ESCAPE CHARACTER!
+    let users = props.fact.users.items;
+    let user = users.find(user => user.employee_id === employee_id); // Returns undefined if not found
+    if (user) {
+      setFieldValue(fieldName, `${employee_id}\\${user.firstname_th} ${user.lastname_th}`, false);
+      return;
+    } else {
+      return 'Invalid Employee ID';
+    }
+  };
+
+  const validateUserEmployeeIDField = (...args) => validateEmployeeIDField("created_by_user_employee_id", ...args);
+  const validateAdminEmployeeIDField = (...args) => validateEmployeeIDField("created_by_admin_employee_id", ...args);
+
+  const validateWarehouseIDField = (fieldName, warehouse_id) => {
+    console.log("I am validating warehouse id")
+    warehouse_id = `${warehouse_id}`.split('\\')[0]; // Escape Character WAREHOUSE_ID CANT HAVE ESCAPE CHARACTER!
+    let warehouses = props.fact.warehouses.items;
+    let warehouse = warehouses.find(warehouse => `${warehouse.warehouse_id}` === `${warehouse_id}`); // Returns undefined if not found
+    if (warehouse) {
+      setFieldValue(fieldName, `${warehouse_id}\\[${warehouse.abbreviation}] ${warehouse.name}`, false);
+      return;
+    } else {
+      return 'Invalid Warehouse ID';
+    }
+  }
+  const validateDestWarehouseIDField = (...args) => validateWarehouseIDField("dest_warehouse_id", ...args);
+
+  return (
+    <div id="blackground-white">
+      <div className="container_12 clearfix">
+        <section className="container_12 ">
+          <h4 className="head-title">นำอะไหล่เข้าโดยมีใบสั่งซื้อ</h4>
+          <div className="container_12">
+
+            {/* Document ID */}
+            <div className="grid_2">
+              <p className="top-text">เลขที่เอกสาร</p>
+            </div>
+            <div className="grid_3 pull_1">
+              <TextInput name='internal_document_id' validate={validateInternalDocumentIDField}
+                searchable={props.toolbar.mode === TOOLBAR_MODE.SEARCH} ariaControls="modalDocument" tabIndex="1" />
+            </div>
+
+            {/* Document Status  */}
+            <div className="grid_3 float-right">
+              <TextInput name="status_name_th" disabled />
+            </div>
+            <div className="grid_2 float-right">
+              <p className="top-text float-right">สถานะ</p>
+            </div>
+          </div>
+
+          <div className="container_12">
+            {/* Created by User */}
+            <div className="grid_2">
+              <p className="top-text">ผู้นำเข้า</p>
+            </div>
+            <div className="grid_3 pull_1">
+              {/* Q: If this is user name in thai, how do we get ID? */}
+              <TextInput name="created_by_user_employee_id" validate={validateUserEmployeeIDField}
+                disabled={props.toolbar.mode === TOOLBAR_MODE.SEARCH}
+                searchable={props.toolbar.mode !== TOOLBAR_MODE.SEARCH} ariaControls="modalUserName" tabIndex="2" />
+            </div>
+
+            {/* Created On */}
+            <div className="grid_3 float-right">
+              <DateTimeInput name="created_on" /*validate={validateCreateOnField */
+                disabled />
+            </div>
+            <div className="grid_2 float-right">
+              <p className="top-text float-right">วันที่</p>
+            </div>
+          </div>
+
+          <div className="container_12">
+            {/* Admin Name */}
+            <div className="grid_2">
+              <p className="top-text">ผู้สร้างเอกสาร</p>
+            </div>
+            <div className="grid_3 pull_1">
+              <TextInput name="created_by_admin_employee_id" validate={validateAdminEmployeeIDField} disabled />
+            </div>
+
+            {/* Document date */}
+            <div className="grid_3 float-right">
+              <DateInput name="document_date"
+                disabled={props.toolbar.mode === TOOLBAR_MODE.SEARCH} tabIndex="3" />
+            </div>
+            <div className="grid_2 float-right">
+              <p className="top-text float-right">วันที่เอกสาร</p>
+            </div>
+
+          </div>
+
+          {/* PO ID */}
+          <div className="container_12">
+            <div className="grid_2">
+              <p className="top-text">เลขที่ใบสั่งซื้อ/เลขที่เอกสารอ้างอิง</p>
+            </div>
+            <div className="grid_3 pull_0">
+              <TextInput name="po_id" disabled={props.toolbar.mode === TOOLBAR_MODE.SEARCH} tabIndex="4" />
+            </div>
+
+            {/* Dest Warehouse ID */}
+            <div className="grid_3 float-right">
+              <TextInput name="dest_warehouse_id" validate={validateDestWarehouseIDField}
+                disabled={props.toolbar.mode === TOOLBAR_MODE.SEARCH}
+                searchable={props.actionMode !== TOOLBAR_MODE.SEARCH} ariaControls="modalInventory" tabIndex="5" />
+            </div>
+            <div className="grid_2 float-right">
+              <p className="top-text float-right">เลขที่คลัง</p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* PopUp ค้นหาเลขที่เอกสาร */}
+      <PopupModalDocument documentTypeGroupID={DOCUMENT_TYPE_ID.GOODS_RECEIPT_PO} 
+      id="modalDocument" //For Open POPUP
+      name="internal_document_id" //For setFieldValue
+      />
+
+      {/* PopUp ค้นหาเลขที่คลัง MODE ADD */}
+      <PopupModalInventory />
+
+      {/* PopUp ค้นหาชื่อพนักงาน MODE ADD */}
+      <PopupModalUsername />
+
+    </div>
+  )
+
+}
+const mapStateToProps = (state) => ({
+  fact: state.api.fact,
+  toolbar: state.toolbar,
+  decoded_token: state.token.decoded_token,
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopContent);
