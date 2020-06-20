46d44ae7b64d84973d62152490e84ba2938b7b73
Knight H <thanapapas@hotmail.com>
2020-06-20 18:20:20 +0700

    added routes for ALS PPM, PM and SS101 summary. References #51

---
+++ src/js/components/als-summary-ss101/index.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from 'react';
+import { scaleLinear } from "d3-scale";
+import { extent } from "d3-array"
+import { useFormik, withFormik, useFormikContext } from 'formik';
+
+import { footerToModeInvisible } from '../../redux/modules/footer.js';
+import { useDispatch, useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom';
+
+import { useToolbarChangeModeInitializer } from '../../hooks/toolbar-initializer';
+import { TOOLBAR_MODE, TOOLBAR_ACTIONS, MODE_TO_ACTION_CREATOR } from '../../redux/modules/toolbar.js';
+import useFactInitializer from '../../hooks/fact-initializer';
+import useTokenInitializer from '../../hooks/token-initializer';
+
+import ScatterPlot from '../als-spare/d3-scatter-plot';
+import LineGraph from '../als-spare/d3-line-graph';
+import BarDivergingGraph from '../als-spare/d3-bar-diverging';
+import Histogram from '../als-equipment-status/d3-histogram';
+import ThailandMapComponent from '../als-equipment-status/d3-map';
+import SimpleGrayCardComponent from '../als-equipment-status/simple-gray-card';
+import AdjustmentBarComponent from '../als-equipment-status/adjustment-bar';
+import EquipmentStatusListComponent from '../als-equipment-status/equipment-status-list';
+
+
+const randomHistogramData = () => {
+    let results = [];
+
+    results.push(0)
+    for (let i = 0; i < 1000; i++) {
+        let randomNumber = (Math.random() + Math.random() + Math.random() + Math.random()) / 4*100; 
+        results.push(randomNumber);
+    }
+
+    return results;
+}
+
+const AlsEquipmentStatusComponent = () => {
+    const dispatch = useDispatch();
+    const loggedIn = useSelector(state => state.token.isLoggedIn);
+
+    // Initializer: Change Toolbar to Mode None
+    useToolbarChangeModeInitializer(TOOLBAR_MODE.NONE_HOME); // TODO: Needs to find where to go when we press "HOME"!!
+    useTokenInitializer();
+    useFactInitializer();
+    useEffect(() => {
+        dispatch(footerToModeInvisible());
+    }, []);
+
+
+
+    return (
+        <>
+            {!loggedIn ? <Redirect to="/" /> : null}
+
+            <div id="blackground-white" >
+                <div className="bootstrap-wrapper">
+                    <div className="container" style={{ marginTop: "70px" }}>
+                        {/* Section Title */}
+                        <h4 className="head-title no-margin">แสดงผลสถานะของสินทรัพย์</h4>
+
+
+                        {/* Columns have horizontal padding to create the gutters between individual columns, however, you can remove the margin from rows and padding from columns with .no-gutters on the .row. */}
+                        <div className="row_bootstrap ">
+                            {/* === Annual Average Inventory Month Line Graph :1st Row, 1st Column === */}
+                            <div className="col-3" >
+                                <SimpleGrayCardComponent
+                                    name="จำนวนสินทรัพย์ทั้งหมด"
+                                    value={2000}
+                                />
+                            </div>
+
+
+                            {/* === Current Average Inventory Month Text :1st Row, 2nd Column === */}
+                            <div className="col-3">
+                                <SimpleGrayCardComponent
+                                    name="จำนวนสินทรัพย์ที่ใช้งาน"
+                                    value={1600}
+                                />
+
+                            </div>
+
+                            {/* === Current Inventory Month vs Planned Inventory Month Scatter Plot :1st Row, 2nd Column === */}
+                            <div className="col-3">
+                                <SimpleGrayCardComponent
+                                    name="จำนวนสินทรัพย์ชำรุด"
+                                    value={98}
+                                />
+
+                            </div>
+
+                            <div className="col-3" >
+                                <SimpleGrayCardComponent
+                                    name="จำนวนสินทรัพย์ดำเนินการซ่อม"
+                                    value={302}
+                                />
+
+                            </div>
+                        </div>
+                        {/*=== Second Row ===*/}
+                        <div className="row_bootstrap no-gutters">
+                            <div className="col-2" >
+                                <AdjustmentBarComponent />
+                            </div>
+                            <div className="col-6"
+                                style={{
+                                    // border:"1px solid red", 
+                                    height: "450px"
+                                }}>
+
+                                <ThailandMapComponent />
+                            </div>
+                            <div className="col-4"
+                                style={{
+                                    // border:"1px solid red", 
+                                    height: "300px"
+                                }}>
+                                <Histogram 
+                                    chartSettings={{ marginLeft: 50, marginTop: 70, marginBottom: 40, height: 300 }} 
+                                    data={randomHistogramData()}
+                                    title="กลุ่มอายุของสินทรัพย์"
+                                    xAxis="อายุการใช้งานของสินทรัพย์"
+                                    yAxis="จำนวนของสินทรัพย์"
+                                />
+
+                                <div class="space-50px" />
+
+                                <EquipmentStatusListComponent />
+                            </div>
+                        </div>
+
+                        {/*=== Third Row ===*/}
+                        <div className="row_bootstrap no-gutters">
+                            <div className="col-4" style={{ border: "1px solid red", height: "200px" }}>
+                                <ScatterPlot />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+const EnhancedAlsEquipmentStatusComponent = withFormik({
+    mapPropsToValues: () => ({
+        equipment_group_id: '',
+        division_id: '',
+        district_id: '',
+        node_id: '',
+    })
+})(AlsEquipmentStatusComponent);
+
+
+export default EnhancedAlsEquipmentStatusComponent;
