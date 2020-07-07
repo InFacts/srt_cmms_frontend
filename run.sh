78daa246e6eecf23b94e1c9403635403cfc4d13a
SorawitSiri <sorawit.pixel@gmail.com>
2020-07-07 21:27:10 +0700

    Test Deploy

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
-docker build -t 127.0.0.1:5000/test:1 .
+docker build -t 127.0.0.1:5000/test:2 .
 docker push 127.0.0.1:5000/test:1
-ssh root@192.168.200.102 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:1
-EOF
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:1
-EOF
-ssh root@192.168.200.102 /bin/bash << EOF
 docker pull 127.0.0.1:5000/test:1
 EOF
 ssh root@192.168.200.103 /bin/bash << EOF
