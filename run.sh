cc0714c07a135fe16969a149f891c5d0fce1ac1f
SorawitSiri <sorawit.pixel@gmail.com>
2020-07-07 21:36:22 +0700

    Test Deploy v3

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-docker build -t 127.0.0.1:5000/test:2 .
-docker push 127.0.0.1:5000/test:1
+docker build -t 127.0.0.1:5000/test:3 .
+docker push 127.0.0.1:5000/test:3
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:1
+docker pull 127.0.0.1:5000/test:3
 EOF
 ssh root@192.168.200.103 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:1
+docker pull 127.0.0.1:5000/test:3
 EOF
 docker stack deploy -c docker-compose.yml frontend
