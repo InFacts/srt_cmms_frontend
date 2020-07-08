fa99c690340b0646e6979822fd40e1f112e09e03
SorawitSiri <sorawit.pixel@gmail.com>
2020-07-08 08:19:19 +0700

    Deploy v5.0

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-docker build -t 127.0.0.1:5000/test:4 .
-docker push 127.0.0.1:5000/test:4
+docker build -t 127.0.0.1:5000/test:5 .
+docker push 127.0.0.1:5000/test:5
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:4
+docker pull 127.0.0.1:5000/test:5
 EOF
 ssh root@192.168.200.103 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:4
+docker pull 127.0.0.1:5000/test:5
 EOF
 docker stack deploy -c docker-compose.yml frontend
