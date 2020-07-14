f2d86a00bd541a618c29e7031d205d16070ad098
SorawitSiri <sorawit.pixel@gmail.com>
2020-07-14 08:58:10 +0700

    Update v8.0

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-docker build -t 127.0.0.1:5000/test:7 .
-docker push 127.0.0.1:5000/test:7
+docker build -t 127.0.0.1:5000/test:8 .
+docker push 127.0.0.1:5000/test:8
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:7
+docker pull 127.0.0.1:5000/test:8
 EOF
 ssh root@192.168.200.103 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:7
+docker pull 127.0.0.1:5000/test:8
 EOF
 docker stack deploy -c docker-compose.yml frontend
