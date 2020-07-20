242542fdf85b7087b278e04c6db616a9c2e28d98
SorawitSiri <sorawit.pixel@gmail.com>
2020-07-20 11:38:47 +0700

    Push V9.0

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-docker build -t 127.0.0.1:5000/test:8 .
-docker push 127.0.0.1:5000/test:8
+docker build -t 127.0.0.1:5000/test:9 .
+docker push 127.0.0.1:5000/test:9
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:8
+docker pull 127.0.0.1:5000/test:9
 EOF
 ssh root@192.168.200.103 /bin/bash << EOF
-docker pull 127.0.0.1:5000/test:8
+docker pull 127.0.0.1:5000/test:9
 EOF
 docker stack deploy -c docker-compose.yml frontend
--- src/js/config_url.js
+++ src/js/config_url.js
@@ -1,1 +1,1 @@
-export const API_URL_DATABASE = "192.168.200.100";
+export const API_URL_DATABASE = "43.229.79.36";
