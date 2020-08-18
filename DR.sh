a923f193d35e7e26719215e908797a3ddb4dff7f
codenuk <supagorn00001@gmail.com>
2020-08-18 15:56:58 +0000

    Test deploy version 4

--- DR.sh
+++ DR.sh
@@ -1,5 +1,5 @@
-docker build -t 127.0.0.1:5000/frontend:3 .
-docker push 127.0.0.1:5000/frontend:3
+docker build -t 127.0.0.1:5000/frontend:4 .
+docker push 127.0.0.1:5000/frontend:4
 docker stack deploy -c docker-compose.yml frontend
 ssh root@192.168.200.103 /bin/bash << EOF
 docker pull 127.0.0.1:5000/frontend:2
