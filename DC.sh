0d7e0f082be72904f6a312b6e79bfc419d4baed1
codenuk <supagorn00001@gmail.com>
2020-08-25 16:08:20 +0000

    deploy v10

--- DC.sh
+++ DC.sh
@@ -1,6 +1,6 @@
-docker build -t 127.0.0.1:5000/frontend:9 .
-docker push 127.0.0.1:5000/frontend:9
+docker build -t 127.0.0.1:5000/frontend:10 .
+docker push 127.0.0.1:5000/frontend:10
 docker stack deploy -c docker-compose.yml frontend
 ssh root@192.168.200.101 /bin/bash << EOF
-docker pull 127.0.0.1:5000/frontend:2
+docker pull 127.0.0.1:5000/frontend:10
 EOF
