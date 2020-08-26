1919f3df8e11ede5ecea9f707544dd6cf6b648b4
Sorawit <sorawti.pixel@gmail.com>
2020-08-26 17:25:45 +0000

    S16/46 have FastTrack only

--- DR.sh
+++ DR.sh
@@ -1,7 +1,7 @@
-docker build -t 127.0.0.1:5000/frontend:14 .
-docker push 127.0.0.1:5000/frontend:14
+docker build -t 127.0.0.1:5000/frontend:15 .
+docker push 127.0.0.1:5000/frontend:15
 docker stack deploy -c docker-compose.yml frontend
 ssh root@192.168.200.103 /bin/bash << EOF
-docker pull 127.0.0.1:5000/frontend:14
+docker pull 127.0.0.1:5000/frontend:15
 EOF
 sleep 10;
