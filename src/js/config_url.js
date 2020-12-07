8a2fe7f722ff16290cc85a11c90d7788b4a125bb
codenuk <supagorn00001@gmail.com>
2020-12-07 14:18:27 +0000

    deploy v16

--- src/js/config_url.js
+++ src/js/config_url.js
@@ -1,2 +1,2 @@
-// export const API_URL_DATABASE = "192.168.200.100";  // ของการรถไฟ
-export const API_URL_DATABASE = "196.52.105.76";  // ของเรา
+export const API_URL_DATABASE = "192.168.200.100";  // ของการรถไฟ
+// export const API_URL_DATABASE = "196.52.105.76";  // ของเรา
